523a268a7dffe08ba38812f268bfae5a826252a8
Bhimgouda <satvikp508@gmail.com>
2022-11-25 13:29:18 +0530

    added feed input options

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import CreateIcon from '@mui/icons-material/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 
 export default function Feed() {
   return (
@@ -11,6 +16,12 @@ export default function Feed() {
                     <input type="text" />
                     <button type='submit'>Send</button>
                 </form>
+            </div>
+            <div className="feed__inputOptions">
+                <InputOption Icon={ImageIcon} title={"Photo"} color="#70b5f9" />
+                <InputOption Icon={SubscriptionsIcon} title={"Photo"} color="#E7A33E" />
+                <InputOption Icon={EventNoteIcon} title={"Photo"} color="#C0CBCD" />
+                <InputOption Icon={CalendarViewDayIcon} title={"Photo"} color="#7FC15E" />
             </div>
         </div>
     </div>
