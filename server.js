756eba0abf2995a68564884bbf9493edd854748e
bhimgouda patil <satvikp508@gmail.com>
2022-12-01 16:15:26 +0530

    deployed on railway

--- server.js
+++ server.js
@@ -17,17 +17,17 @@ const dbUrl = process.env.MONGODB_URI || 'mongodb+srv://bhim511:HikeHike55$$@clu
 // -------------------------- MIDDLEWARES -----------------------------------//
 
 // req.body parsers
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
 
 // -------------------------- REALTIME SOCKET.IO CONNECTION ---------------------------- //
 
-io.on("connection", async(socket)=>{
+io.on("connection", async (socket) => {
     const posts = await getAllPosts()
     socket.emit('send-posts', posts)
-    socket.on('add-post', async(post)=>{
+    socket.on('add-post', async (post) => {
         const p = await addPost(post)
         io.emit("update-feed", p);
     })
@@ -70,25 +70,25 @@ app.use(session(sessionConfig))
 app.use('/api', userRouter)
 
 app.use(express.static("client/build"));
-app.get("/*",(req,res)=>{
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 })
 
 // -------------------------------------- Error handling middleware ----------------------//
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     console.log(err.stack);
-    const {message="Something went wrong", status=500} = err;
+    const { message = "Something went wrong", status = 500 } = err;
     res.status(status).send(message);
 })
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`Server Listening on port ${PORT}`)
 })
 
 mongoose.connect(dbUrl)
-.then(console.log("DB Connected"))
-.catch(e=>console.log(e))
+    .then(console.log("DB Connected"))
+    .catch(e => console.log(e))
 
 
 
